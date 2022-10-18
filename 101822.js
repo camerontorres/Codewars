e032dadcb60d68a2e246ec0ec146987bf07f83f9
Cameron <camerontorres759@gmail.com>
2022-10-18 16:09:46 -0400

    Create 101822.js

---
+++ 101822.js
@@ -0,0 +1,3 @@
+function sortByLength (array) {
+  return array.sort((a, b) => a.length - b.length)    //we are returning a sorted array by the length of each element to give us the sorted output
+};
