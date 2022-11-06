fc9bc722cc8dfb97ba0d33630aa3b7f22d981e9b
Cameron <camerontorres759@gmail.com>
2022-11-05 21:30:23 -0400

    Create 110522.js

---
+++ 110522.js
@@ -0,0 +1,17 @@
+function switchItUp(number){
+let num = { //pretty basic, we're creating an array of objects that are numbers that are equal to their string equivalent 
+0: 'Zero',
+  1: 'One',
+  2: 'Two',
+  3: 'Three',
+  4: 'Four',
+  5: 'Five',
+  6: 'Six',
+  7: 'Seven',
+  8: 'Eight',
+  9: 'Nine'
+  
+
+}
+return num[number] //we return the object array with whatever is in "number" as what we want out of it
+}
