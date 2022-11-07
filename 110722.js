df17a403f8bdf2ab4e303ab0ebf227f931672c02
Cameron <camerontorres759@gmail.com>
2022-11-07 14:42:05 -0500

    Create 110722.js

---
+++ 110722.js
@@ -0,0 +1,9 @@
+var summation = function (num) {
+  let sum = 0. //create an empty sum
+  for( let i = 0; i <= num; i++){ //start a loop that begins at zero and goes until it equals the inputted num
+      sum += i //add and total all numbers i the loop to the sum
+   
+  }
+  return sum // return
+  
+}
