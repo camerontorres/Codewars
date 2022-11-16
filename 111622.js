88501b58f06d8d47082b609533020b4dad36677b
Cameron <camerontorres759@gmail.com>
2022-11-16 22:00:28 +0100

    Create 111622.js

---
+++ 111622.js
@@ -0,0 +1,14 @@
+function makeNegative(num) {
+  if(num <= 0){       //if num is less than or equals zero, we will just return num
+    return num
+  }else{
+  return num - (num*2)}     //else, we wil subtract the value of num twice, giving us a negative number.
+  
+  
+  
+  
+  
+  //seeing the solutions after i finished, I now know i can simply return (-num) to return a negative value, this would have been a little simpler.
+  
+  
+}
